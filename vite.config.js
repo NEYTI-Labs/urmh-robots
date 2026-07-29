2f7aaf478fc7f6bd5af3c0b9419ad9c4a4edefaf
nikitadmtr <nikitabezhinarov@mail.ru>
2026-07-30 01:02:32 +0800

    fixs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
+  base: '/urmh-robots/',
   plugins: [
     handlebars({
       partialDirectory: './partials',
