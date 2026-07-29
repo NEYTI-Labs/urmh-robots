d073d9c70d471273b7661bc3a9017b4e5e36d722
nikitadmtr <nikitabezhinarov@mail.ru>
2026-07-30 02:10:29 +0800

    fixs

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/urmh-robots/',
+
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/index.js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith('.css')) {
+            return 'assets/index.css';
+          }
+          return 'assets/[name][extname]';
+        },
+      },
+    },
+  },
+
   plugins: [
     handlebars({
-      partialDirectory: './partials',
+      partialDirectory: resolve(__dirname, 'partials'),
     }),
   ],
 });
